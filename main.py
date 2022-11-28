97a7e4ee381960230a5be99c8476f0662d496d6b
Slyce Anguilde <91631241+SlyceDF@users.noreply.github.com>
2022-11-28 13:13:38 +0000

    reset everything

--- main.py
+++
@@ -1,6 +0,0 @@
-#numa = eval(input("Please input Number I:\n"))
-#numb = eval(input("Please input Number II:\n"))
-#print("Check this out: "+str(numa*numb))
-def root(a, b):
-  return b**(1.0/a)
-print(root(2.0,-1.0))
