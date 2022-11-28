5d202565b3fe64aba35b38080b7096057907c377
Slyce Anguilde <91631241+SlyceDF@users.noreply.github.com>
2022-11-28 16:19:44 +0000

    Now in style

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ import logging
 import os
 
 app = Flask(  # Create a flask app
-	__name__,
-	template_folder='frontend',  # Name of directory for template files
+  __name__,
+  template_folder='frontend',  # Name of directory for template files
   static_folder='frontend'  # Name of directory for static files
-
 )
 
-
-#@app.route('/',  methods=['GET', 'POST'])
+#@app.route('/', methods=['GET', 'POST'])
 #def index():
 #  return render_template('index.html')
 
 #@app.errorhandler(404)
 #def page_not_found(e):
-    #return render_template('404.html'), 404
+#return render_template('404.html'), 404
 
 #@app.errorhandler(500)
 #def server_error(e):
-    #return render_template('500.html'), 500
+#return render_template('500.html'), 500
